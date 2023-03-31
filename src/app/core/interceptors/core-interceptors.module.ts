64f81d0a225a856e603a0681aee0b9201551ba27
Marco <marcoross37@gmail.com>
2023-03-31 16:47:16 +0200

    hub-19: Initial home screen. - Added home screen with azure DevOps items on left and desc on right. - Added some responsiveness. - Added forbidden interceptor to log users out. - Added stylings and colours. - Added items on page init.

--- src/app/core/interceptors/core-interceptors.module.ts
+++ src/app/core/interceptors/core-interceptors.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ApiForbiddenInterceptorProviders } from './api-forbidden.interceptor';
 import { ApiRouteInterceptorProviders } from './api-route.interceptor';
 import { ApiServerErrorInterceptorProviders } from './api-server-error.interceptor';
 import { AuthInterceptorProviders } from './authentication.interceptor';
@@ -7,7 +8,7 @@ import { ServerOfflineInterceptorProviders } from './server-offline.interceptor'
 @NgModule({
   declarations: [],
   imports: [],
-  providers: [ApiRouteInterceptorProviders, ServerOfflineInterceptorProviders, ApiServerErrorInterceptorProviders, AuthInterceptorProviders],
+  providers: [ApiRouteInterceptorProviders, ServerOfflineInterceptorProviders, ApiServerErrorInterceptorProviders, ApiForbiddenInterceptorProviders, AuthInterceptorProviders],
   exports: []
 })
 export class CoreInterceptorsModule { }
--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -1,20 +1,44 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { HomeService } from './home.service';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'home',
     templateUrl: 'home.component.html',
     styleUrls: ['home.component.scss'],
-    providers: []
+    providers: [HomeService]
 })
 export class HomeComponent
 {
-    constructor() { }
+    constructor(private homeService: HomeService) { }
 
-    homeFG!: FormGroup;
+    faExternalLink = faExternalLink;
 
-    ngOnInit(){
-        
+    //
+
+    workItems: any = [];
+
+    selectedWorkItem: any;
+
+    ngOnInit()
+    {
+        this.homeService.GetAllWorkItems().subscribe({
+            next: (workItemsQuery) =>
+            {
+                this.workItems = workItemsQuery.workItems.sort((x: any, y: any) => new Date(y.fields.changedDate).getTime() - new Date(x.fields.changedDate).getTime());
+            }
+        });
+    }
+
+    getItemDescription()
+    {
+        if (!this.selectedWorkItem)
+            return 'Select a work item on the left to view it\'s details.';
+
+        if (!this.selectedWorkItem?.fields?.description)
+            return 'A description has not been provided.';
+
+        return this.selectedWorkItem?.fields?.description;
     }
 
 }
