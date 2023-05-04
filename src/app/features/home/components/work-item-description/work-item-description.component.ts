af3927a62e51fad6bb280b0d53cfd2132311a8e5
Marco <marcoross37@gmail.com>
2023-05-04 12:13:40 +0200

    Hub-44: Make home responsive. - Added new component for work item description. - Made home responsive for mobile. - Added popup of description on mobile. - Fixed styling.

---
+++ src/app/features/home/components/work-item-description/work-item-description.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'work-item-description',
+    templateUrl: 'work-item-description.component.html',
+    styleUrls:['work-item-description.component.scss'],
+    providers: []
+})
+export class WorkItemDescriptionComponent
+{
+    constructor() { }
+
+    @Input() selectedWorkItem: any;
+
+    public GetItemDescription()
+    {
+        if (!this.selectedWorkItem)
+            return 'Select a work item on the left to view it\'s details.';
+
+        if (!this.selectedWorkItem?.fields?.description)
+            return 'A description has not been provided.';
+
+        return this.selectedWorkItem?.fields?.description;
+    }
+}
