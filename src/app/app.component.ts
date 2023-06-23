d7eb30d2356288231929bbe86f97c1fa11914525
Marco <marcoross37@gmail.com>
2023-06-23 18:40:32 +0200

    Added background color while page loads.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ThemeRenderer } from './global-shared/services/theme/theme.renderer';
 import { LoggedInCookie } from './global-shared/services/cookies/logged-in.cookie';
+import { WindowRefService } from './global-shared/services/window/window-ref.service';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,14 @@ export class AppComponent
   title = 'my-hub-web';
   readonly ThemeKey = 'Theme';
 
-  constructor(private themeRenderer: ThemeRenderer, private loggedInCookie: LoggedInCookie) { }
+  constructor(private themeRenderer: ThemeRenderer, private loggedInCookie: LoggedInCookie, private windowRefService: WindowRefService) { }
 
   themeLoading: boolean = false;
 
   ngOnInit()
   {
+    this.windowRefService.nativeWindow.document.documentElement.style.cssText = '';
+
     this.SetTheme();
 
     this.themeRenderer.getIsThemeLoading().subscribe({
