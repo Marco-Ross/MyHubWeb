6b870c284b068246c9ef97899e07847369e6c1ae
Marco <marcoross37@gmail.com>
2023-08-31 12:43:04 +0200

    hub-80: About page fixes - Added copy to clipboard for email. - Updated styling. - Removed background.

--- src/app/features/about/components/about/about.component.ts
+++ src/app/features/about/components/about/about.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Container, Engine, MoveDirection, OutMode } from 'tsparticles-engine';
-import { loadSlim } from "tsparticles-slim";
-
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'about',
@@ -13,9 +12,13 @@ export class AboutComponent
 {
     constructor(private formBuilder: FormBuilder) { }
 
+    faEnvelope = faEnvelope;
+
     //
 
     aboutFG!: FormGroup;
+    copyText: string = 'Copy email';
+    autoClose = false;
 
     ngOnInit()
     {
@@ -25,98 +28,16 @@ export class AboutComponent
 
     //////
 
-
-    id = "tsparticles";
-
-    /* or the classic JavaScript object */
-    particlesOptions = {
-        fullScreen:{
-            // enable: true,
-            zIndex: -1
-        },
-        background: {
-            // color: {
-            //     value: "transparent",
-            // },
-        },
-        fpsLimit: 60,
-        interactivity: {
-            events: {
-                // onClick: {
-                //     enable: true,
-                //     mode: ClickMode.push,
-                // },
-                // onHover: {
-                //     enable: true,
-                //     mode: HoverMode.repulse,
-                // },
-                resize: true,
-            },
-            modes: {
-                push: {
-                    quantity: 4,
-                },
-                repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                },
-            },
-        },
-        particles: {
-            color: {
-                value: "#4c4c4c",
-            },
-            links: {
-                color: "#4c4c4c",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-            },
-            move: {
-                direction: MoveDirection.none,
-                enable: true,
-                outModes: {
-                    default: OutMode.bounce,
-                },
-                random: false,
-                speed: 2,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800,
-                },
-                value: 80,
-            },
-            opacity: {
-                value: 0.5,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: { min: 1, max: 5 },
-            },
-        },
-        detectRetina: true,
-    };
-
-    particlesLoaded(container: Container): void
+    copyEmail()
     {
-        // console.log(container);
+        this.copyText = 'Email Copied';
     }
 
-    async particlesInit(engine: Engine): Promise<void>
+    revertEmail()
     {
-        // console.log(engine);
-
-        // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
-        await loadSlim(engine);
+        setTimeout(() =>
+        {
+            this.copyText = 'Copy email';
+        }, 200);
     }
-
 }
