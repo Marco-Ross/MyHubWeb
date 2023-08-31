d1743d1d836152ce9ce79966d4f5de907af3518b
Marco <marcoross37@gmail.com>
2023-08-31 13:09:04 +0200

    hub-67: removed comment

--- src/app/features/gallery/components/gallery/gallery.component.ts
+++ src/app/features/gallery/components/gallery/gallery.component.ts
@@ -170,7 +170,6 @@ export class GalleryComponent
 
     viewComments(image: IImage)
     {
-        //Image blobs are left behind if (opened and closed) twice and then refresh.
         let options = new uploadOptions('Comments');
         options.buttonText = 'Close';
         options.buttonType = 'Button';
